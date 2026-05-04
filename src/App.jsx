a4a2edc93ad48c68f70cf3c372e95fabf79dd06d
FDAsico <198682449+FDAsico@users.noreply.github.com>
2026-05-05 01:21:36 +0800

    Fixed Login not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Login from './pages/login'
 import Dashboard from './pages/Dashboard'
 import Patients from './pages/Patients'
 import Appointment from './pages/Appointment'
